899b4c9228594d229fec2d7b44a0e9ceeba6c0fc
Michael Mottola <mottola64@gmail.com>
2020-02-24 18:37:58 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,15 @@
-// Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = props => {
+  return (
+    <ul className="review-list">
+      {props.reviews.map(review => (
+        <li className="review">
+          <a href={review.link.url}>{review.headline}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieReviews;
